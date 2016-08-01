7fa70b04980b114fe48dc95fb346fa900c59a567
XinLi96 <291750956@qq.com>
2016-08-01 21:50:24 +0800

    23

---
+++ php/day1/test10.php
@@ -0,0 +1,18 @@
+<?php 
+	$homepage  =  file_get_contents ( 'http://www.example.com/' );
+	echo  $homepage ;
+ ?>
+ <form method="post" accept-charset="utf-8">
+	<input type="submit" name="sub1" value="同意" onclick="agree()"/>
+	<input type="submit" name="sub2" value="不同意" onclick="disagree()"/>
+</form>
+<script>
+	function agree(){
+		//alert('注册成功');
+		document.forms.action = 'next.php';
+	}
+	function disagree(){
+		//alert('正在退出...');
+		document.forms.action = 'index.php';
+	}
+</script>
